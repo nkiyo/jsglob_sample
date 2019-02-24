53f87b24d7c63051925731490056bdcc1d4d2df3
nkiyo <nkiyo@hoge.com>
2019-02-25 08:09:31 +0900

    up

--- sample.js
+++ sample.js
@@ -1,8 +1,22 @@
 const glob = require('glob');
 
-glob('**/d.txt', (err, files) => {
-    for(const i in files) {
-        console.log(`${files[i]}`);
-    }
-});
+const prefix = 'abc';
+const id = '+([0-9])'; // => abc_123.txt, abc_12345.txt, ..
+//const id = '+([a-z])'; // => abc_def.txt
+//const id = '???';
+const pattern = `**/${prefix}_${id}.txt`;
+
+//const pattern = '**/d.txt';
+//glob(pattern, (err, files) => {
+//    console.log(`${files.length} file(s) has found.`);
+//    for(const i in files) {
+//        console.log(`${files[i]}`);
+//    }
+//});
+
+files = glob.sync(pattern);
+console.log(`${files.length} file(s) has found.`);
+for(const i in files) {
+    console.log(`${files[i]}`);
+}
 
